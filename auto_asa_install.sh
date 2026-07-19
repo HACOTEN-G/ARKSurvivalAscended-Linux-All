abf6df5971ccbf7a1cf8bb18d3ed30aa2c1aebf7
HACOTEN-G <okmtestai@gmail.com>
2026-07-19 19:25:14 +0900

    Update auto_asa_install.sh
    fix

--- auto_asa_install.sh
+++ auto_asa_install.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-set -e
-
-echo "=== ARKSurvivalAscended-Linux-All をダウンロードします ==="
-
 git clone https://github.com/HACOTEN-G/ARKSurvivalAscended-Linux-All.git
-
 cd ARKSurvivalAscended-Linux-All
-
-echo "=== server-install-ubuntu20.sh を実行します ==="
-
 bash server-install-ubuntu20.sh
-
-echo "=== インストールが完了しました ==="
